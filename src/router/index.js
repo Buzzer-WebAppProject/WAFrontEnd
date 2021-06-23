29e037ab23ce623558c79abdf983fbd1e4df7aee
DavidMaglica <davidmaglica@protonmail.ch>
2021-06-23 12:09:22 +0200

    minor changes + prettier format

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,49 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Landing.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Landing.vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
-Vue.use(VueRouter)
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'landing',
-    component: Home
+    path: "/",
+    name: "landing",
+    component: Home,
   },
   {
-    path: '/Login',
-    name: 'Login',
-    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+    path: "/Login",
+    name: "Login",
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/Login.vue"),
   },
   {
-    path: '/Signup',
-    name: 'Signup',
-    component: () => import(/* webpackChunkName: "signup" */ '../views/Signup.vue')
+    path: "/Signup",
+    name: "Signup",
+    component: () =>
+      import(/* webpackChunkName: "signup" */ "../views/Signup.vue"),
   },
   {
-    path: '/MainPage',
-    name: 'MainPage',
-    component: () => import(/* webpackChunkName: "signup" */ '../views/MainPage.vue')
+    path: "/MainPage",
+    name: "MainPage",
+    component: () =>
+      import(/* webpackChunkName: "signup" */ "../views/MainPage.vue"),
   },
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
