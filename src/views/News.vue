53ebe851ef68aa78dbeeeb465264e101ea427377
David Kostić <davidkostic55@gmail.com>
2021-09-15 13:46:31 +0200

    news and thruAges content added

--- src/views/News.vue
+++ src/views/News.vue
@@ -6,41 +6,103 @@
         <div class="col-2" />
 
         <div class="col-6 mid">
-          <h1>News</h1>
+          <h1>Bees in the News</h1>
 
           <p class="intro">
-            Like every other animal or insect, honeybees are affected by pests
-            and parasites. Perhaps the most common enemy of the honeybee is the
-            varroa mite.
-          </p>
-          <p class="intro">
-            Manufacturers have developed a variety of products to treat and get
-            rid of these pests and parasites. Read our reviews to find the best
-            product for your bees.
+            Welcome to the latest news about honeybees and everything related to them!
           </p>
 
           <br />
 
           <div class="paragraph">
-            <div class="dropdown-divider-middle" />
+            <div class="dropdown-divider" />
 
             <h2 class="ArtTitle">
-              Beekeeping Barrier Management System for Pest and Disease Control
+              Bees Needs Week
             </h2>
 
             <img
               class="image"
               alt="image"
-              src="@/assets/PostImages/varroosis.jpg"
+              src="@/assets/PostImages/news1.jpg"
             />
             <p class="text">
-              A structured approach in barrier management gives you the best
-              results in pest and disease control. It allows you unique insights
-              into the barriers you establish and their effectiveness. You are
-              also able to follow up and monitor performance of the barriers
-              with ease. The barrier management system for pest and disease
-              control in your beekeeping operation makes the operation safe,
-              with your better understanding of risks.
+              What is Bees Needs Week?
+              Bees Needs Week is an annual event coordinated by Defra, working alongside charities, businesses, conservation groups and academic institutions to raise awareness of bees and other pollinators, This year due to Covid19 it will again be an online event by sharing stories and videos through social media. So there won’t be a pop-up shop in Carnaby this year given the restrictions.
+              Raising awareness of bees and other pollinators
+              Bees’ and other pollinators are vital for growing lots of our favourite foods and for plants to flourish in our fields and gardens
+
+ 
+            </p>
+
+            <br />
+          </div>
+
+          <div class="paragraph">
+            <div class="dropdown-divider" />
+
+            <h2 class="ArtTitle">
+              Reporting illegal medicines 
+            </h2>
+
+            <img
+              class="image"
+              alt="image"
+              src="@/assets/PostImages/news2.jpg"
+            />
+            <p class="text">
+              The Veterinary Medicine Directorate’s (VMD) have become aware of a number of unauthorised veterinary medicines/treatments 
+              being advertised and sold for a variety of honey bee issues including:- Varroa, Nosema disease (nosemosis) and Tracheal mites (acarapisosis).
+              We advise members to always use authorised products and also to submit a complaint if they see unauthorised veterinary medicine or products being advertise
+            </p>
+
+            <br />
+          </div>
+
+          <div class="paragraph">
+            <div class="dropdown-divider" />
+
+            <h2 class="ArtTitle">
+              Starvation & Foulbrood Warning
+            </h2>
+
+            <img
+              class="image"
+              alt="image"
+              src="@/assets/PostImages/news3.jpg"
+            />
+            <p class="text">
+              The BBKA is issuing a warning to beekeepers to check their bees for starvation and European Foulbrood during this cold snap. 
+              Emergency feeding may be needed. It would be advisable to check the food levels by opening the hive and making a very quick 
+              observation of their store levels.
+              It is also important to check that hives have small entrances as Foulbrood is about in some areas and small colonies or those 
+              suffering disease may not be able to defend themselves from infected intruders if they have a large opening to defend. 
+              Strong colonies may try to rob weaker ones and thus contract the infections. It is probably not possible to inspect the colonies 
+              as it is still too cold, but the best defence is to ensure they all have food. You can make a careful inspection of their brood to 
+              check for infections when it is warm enough. 
+            </p>
+
+            <br />
+          </div>
+          <div class="paragraph">
+            <div class="dropdown-divider" />
+
+            <h2 class="ArtTitle">
+              Petition to stop importing honeybees
+            </h2>
+
+            <img
+              class="image"
+              alt="image"
+              src="@/assets/PostImages/news4.jpg"
+            />
+            <p class="text">
+              The problem is that at least one beekeeping business has said that they want to import bees to the UK through Northern Ireland. 
+              The bees they are sourcing come from southern Italy where a serious invasive species and parasite of bees, the Small Hive Beetle 
+              is established. The inspections on bees are extremely limited and we are fearful that a small, clubbed antenna, beetle will be able 
+              to evade their scrutiny and be imported to the mainland. 
+
+
             </p>
 
             <br />
@@ -59,7 +121,7 @@
 }
 
 h1 {
-  color: white;
+  color: goldenrod;
 }
 
 .dropdown-divider {
@@ -102,7 +164,7 @@ import Header from "../components/Header.vue";
 import store from "@/store.js";
 
 export default {
-  name: "News",
+  name: "PnP",
   data() {
     return {};
   },
--- src/views/ThruAges.vue
+++ src/views/ThruAges.vue
@@ -9,14 +9,7 @@
           <h1>History of apiculture through the ages</h1>
 
           <p class="intro">
-            Like every other animal or insect, honeybees are affected by pests
-            and parasites. Perhaps the most common enemy of the honeybee is the
-            varroa mite.
-          </p>
-          <p class="intro">
-            Manufacturers have developed a variety of products to treat and get
-            rid of these pests and parasites. Read our reviews to find the best
-            product for your bees.
+            The history of bees and flowers is a fascinating one. So, too, is the history of beekeeping.
           </p>
 
           <br />
@@ -25,72 +18,56 @@
             <div class="dropdown-divider" />
 
             <h2 class="ArtTitle">
-              Beekeeping Barrier Management System for Pest and Disease Control
+              Beekeeping Throughout the Ages
             </h2>
-
             <img
               class="image"
               alt="image"
-              src="@/assets/PostImages/PnP-first.jpg"
+              src="@/assets/PostImages/beeage1.png"
             />
             <p class="text">
-              A structured approach in barrier management gives you the best
-              results in pest and disease control. It allows you unique insights
-              into the barriers you establish and their effectiveness. You are
-              also able to follow up and monitor performance of the barriers
-              with ease. The barrier management system for pest and disease
-              control in your beekeeping operation makes the operation safe,
-              with your better understanding of risks.
+              Beekeeping is an old practice with depictions of it found in art and artifacts from 10,000 years ago. Early beekeepers 
+              of that age used pottery vessels to house honey bee colonies. They developed on to making simple hives and smoke in beekeeping. 
+              Later, the storage of honey in jars came about
+              In the 18th century, Europeans took a lot of interest in beekeeping. They studied the life cycle and biology of honey bees with 
+              the intent to understand honey bee colony organization. Improved understanding led to the discovery and exploitation of ‘bee space’. 
+              It gave rise to movable frames in beehives, and the artificial beehives commonly used in beekeeping to date.
+              Throughout history, beekeeping has been practiced using skeps, pottery vessels, log hives, top bar hives, Warré hives, and the 
+              Langstroth beehive. Of these, the Warré, Langstroth and Top Bar hives are the most popular to date. We’ll take a quick look at these 
+              later on in this article.
             </p>
-
-            <br />
-          </div>
-
-          <div class="paragraph">
-            <div class="dropdown-divider" />
-
             <h2 class="ArtTitle">
-              Beekeeping Barrier Management System for Pest and Disease Control
+              Modern Advancements in Beekeeping
             </h2>
-
-            <img
-              class="image"
-              alt="image"
-              src="@/assets/PostImages/PnP-first.jpg"
-            />
             <p class="text">
-              A structured approach in barrier management gives you the best
-              results in pest and disease control. It allows you unique insights
-              into the barriers you establish and their effectiveness. You are
-              also able to follow up and monitor performance of the barriers
-              with ease. The barrier management system for pest and disease
-              control in your beekeeping operation makes the operation safe,
-              with your better understanding of risks.
+              In recent years, a modification of the Langstroth beehive has been made and is marketed as a Flow hive. Flow hives use proprietary 
+              technologies that allow for harvesting of honey without removing beehive frames from the beehive. The frames are specially made for the 
+              Flow hive. With Flow hives, there is little possibility of harvesting some of the other beehive products available to other beekeepers. 
+              Most notably, using a purely Flow hive setup will see you have very little beeswax to harvest at the end of each year of beekeeping. 
+              On the other hand, Flow hives give you very clean honey with little or no impurities, and save the beekeeper a lot of time. The costs 
+              of buying a honey extractor are also avoided.
+              Until recently, smoke has been used by beekeepers to keep honey bees from stinging the beekeeper on hive visits. Before the rise of 
+              the beekeeping veil, beekeeping was a rather hands-off practice with beehive visits mainly aimed at finding out if the bees have stored 
+              honey, how much it is, and harvesting it. Modern beekeeping requires more frequent monitoring and management of the honey bee colony, and 
+              the beehive in which the colony is housed.
             </p>
-
-            <br />
-          </div>
-
-          <div class="paragraph">
-            <div class="dropdown-divider" />
-
             <h2 class="ArtTitle">
-              Beekeeping Barrier Management System for Pest and Disease Control
+              Advancements in Beehives
             </h2>
-
             <img
               class="image"
               alt="image"
-              src="@/assets/PostImages/PnP-first.jpg"
+              src="@/assets/PostImages/beeage2.jpg"
             />
             <p class="text">
-              A structured approach in barrier management gives you the best
-              results in pest and disease control. It allows you unique insights
-              into the barriers you establish and their effectiveness. You are
-              also able to follow up and monitor performance of the barriers
-              with ease. The barrier management system for pest and disease
-              control in your beekeeping operation makes the operation safe,
-              with your better understanding of risks.
+              Beekeeping today is mainly done using beehives. These beehives have come a long way over time and seen some evolution into what they are today.
+              Modern beehives also employ concepts that make production easy, and allow for targeting of some beehive products over others. This progress in
+              the structure used to house honey bees is good and enables continuity of the honey bee colony even after beehive products are harvested.
+              Previous beehives, as recent as the log hive, made continuous survival of the honey bee colony difficult because harvesting honey would ruin a 
+              lot of the brood comb which is usually near the entrance of the beehive. Today, the major beehives used in beekeeping are the Langstroth beehive, 
+              Top Bar hive, the Warré hive, the British standard hive. Others which may not be as popular but still have decent utilization are the Layens hive 
+              and the Dadant hive. The suitability of these beehives varies depending on the preferences of individual beekeepers, and whether the beehive has a 
+              vertical or horizontal orientation.
             </p>
 
             <br />
@@ -109,7 +86,7 @@
 }
 
 h1 {
-  color: white;
+  color: goldenrod;
 }
 
 .dropdown-divider {
